e9cf3e5b36b583db583bead0768d2d76be995d46
Huang Degil <huang.degil@gmail.com>
2018-10-29 10:40:14 +0900

    Added homework solutions

---
+++ Homework3/Homework3/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace Homework3 {
+  public partial class MainWindow : Window {
+    public MainWindow() {
+      InitializeComponent();
+    }
+
+    private void MyButton_Click(object sender, RoutedEventArgs e) {
+      CarDependencyProperty dpSample = TryFindResource("CarDependency") as CarDependencyProperty;
+      MessageBox.Show(dpSample.MyCar);
+    }
+  }
+
+  public class CarDependencyProperty : DependencyObject {
+    public static readonly DependencyProperty CarDependency = DependencyProperty.Register("MyProperty", typeof(string), typeof(CarDependencyProperty));
+
+    public string MyCar {
+      get {
+        return (string) GetValue(CarDependency);
+      }
+      set {
+        SetValue(CarDependency, value);
+      }
+    }
+  }
+}
