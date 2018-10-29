888e574b122a69f1acdb51770fd54b649ed34edd
Huang Degil <huang.degil@gmail.com>
2018-10-29 12:04:57 +0900

    10-29

---
+++ Command_176/Command_176/Emp.cs
@@ -0,0 +1,11 @@
+﻿namespace Command_176 {
+  class Emp {
+    public string Ename { get; set; }
+    public string Job { get; set; }
+    public string Detail {
+      get {
+        return "[" + Ename + ", " + Job + "]";
+      }
+    }
+  }
+}
---
+++ Command_176/Command_176/MainWindow.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace Command_176 {
+  public partial class MainWindow : Window {
+    public MainWindow() {
+      InitializeComponent();
+    }
+  }
+}
