457a1d2153455c1cc4d6d8e379c4cf04cbd1dce5
Huang Degil <huang.degil@gmail.com>
2018-11-01 05:07:59 +0900

    Refactored

--- Calculator_185/Calculator_185/Calculator.cs
+++ Calculator_185/Calculator_185/Calculator.cs
@@ -4,25 +4,12 @@ using System.Windows.Input;
 
 namespace Calculator {
   public class Calculator : INotifyPropertyChanged {
-    public ICommand Append    { protected set; get; }
-    public ICommand Backspace { protected set; get; }
-    public ICommand Clear     { protected set; get; }
-    public ICommand Operator  { protected set; get; }
-    public ICommand Calculate { protected set; get; }
-
-    public Calculator() {
-      Append    = new Append(this);
-      Backspace = new Backspace(this);
-      Clear     = new Clear(this);
-      Operator  = new Operator(this);
-      Calculate = new Calculate(this);
+    protected void OnPropertyChanged(string propertyName) {
+      if (PropertyChanged != null) {
+        PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+      }
     }
-
-    public string  Op  { get; set; }  // Opertaor
-    public double? Op1 { get; set; }  // Operand 1
-
-    string inputString = "";
-    string displayText = "";
+    public event PropertyChangedEventHandler PropertyChanged;
 
     public string InputString {
       internal set {
@@ -36,7 +23,6 @@ namespace Calculator {
       }
       get { return inputString; }
     }
-
     public string DisplayText {
       internal set {
         if (displayText != value) {
@@ -46,53 +32,51 @@ namespace Calculator {
       }
       get { return displayText; }
     }
+    private string inputString = "";
+    private string displayText = "";
 
-    public event PropertyChangedEventHandler PropertyChanged;
+    public string  Op  { get; set; }  // Opertaor
+    public double? Op1 { get; set; }  // Operand 1
 
-    protected void OnPropertyChanged(string propertyName) {
-      if (PropertyChanged != null) {
-        PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-      }
+    public Calculator() {
+      Append    = new Append(this);
+      Backspace = new Backspace(this);
+      Clear     = new Clear(this);
+      Operator  = new Operator(this);
+      Calculate = new Calculate(this);
     }
+    public ICommand Append    { protected set; get; }
+    public ICommand Backspace { protected set; get; }
+    public ICommand Clear     { protected set; get; }
+    public ICommand Operator  { protected set; get; }
+    public ICommand Calculate { protected set; get; }
   }
 
   class Append : ICommand {
-    private Calculator c;
-    
-    public Append(Calculator c) {
-      this.c = c;
-    }
-
     public event EventHandler CanExecuteChanged {
       add    { CommandManager.RequerySuggested += value; }
       remove { CommandManager.RequerySuggested -= value; }
     }
-
     public bool CanExecute(object parameter) {
       return c.InputString.IndexOf('.') < 0 || (string) parameter != ".";
     }
-
     public void Execute(object parameter) {
       c.InputString += parameter;
     }
+    public Append(Calculator c) {
+      this.c = c;
+    }
+    private Calculator c;
   }
 
   class Backspace : ICommand {
-    private Calculator c;
-
-    public Backspace(Calculator c) {
-      this.c = c;
-    }
-
     public event EventHandler CanExecuteChanged {
       add    { CommandManager.RequerySuggested += value; }
       remove { CommandManager.RequerySuggested -= value; }
     }
-
     public bool CanExecute(object parameter) {
       return 0 < c.InputString.Length;
     }
-
     public void Execute(object parameter) {
       int length = c.InputString.Length - 1;
       if (0 < length) {
@@ -101,48 +85,40 @@ namespace Calculator {
         c.InputString = c.DisplayText = "";
       }
     }
+    public Backspace(Calculator c) {
+      this.c = c;
+    }
+    private Calculator c;
   }
 
   class Clear : ICommand {
-    private Calculator c;
-
-    public Clear(Calculator c) {
-      this.c = c;
-    }
-
     public event EventHandler CanExecuteChanged {
       add    { CommandManager.RequerySuggested += value; }
       remove { CommandManager.RequerySuggested -= value; }
     }
-
     public bool CanExecute(object parameter) {
       return 0 < c.InputString.Length;
     }
-
     public void Execute(object parameter) {
       c.InputString = c.DisplayText = "";
       c.Op1 = null;
     }
+    public Clear(Calculator c) {
+      this.c = c;
+    }
+    private Calculator c;
   }
 
   class Operator : ICommand {
-    private Calculator c;
-
-    public Operator(Calculator c) {
-      this.c = c;
-    }
-
     public event EventHandler CanExecuteChanged {
       add    { CommandManager.RequerySuggested += value; }
       remove { CommandManager.RequerySuggested -= value; }
     }
-
     public bool CanExecute(object parameter) {
       double op1;
       return c.InputString == "" && (string) parameter == "-" ||
              c.Op1 == null && double.TryParse(c.InputString, out op1);
     }
-
     public void Execute(object parameter) {
       if (c.InputString == "") {
         c.InputString = "-";
@@ -152,32 +128,27 @@ namespace Calculator {
         c.InputString = "";
       }
     }
+    public Operator(Calculator c) {
+      this.c = c;
+    }
+    private Calculator c;
   }
 
   class Calculate : ICommand {
-    private Calculator c;
-
-    public Calculate(Calculator c) {
-      this.c = c;
-    }
-
     public event EventHandler CanExecuteChanged {
       add    { CommandManager.RequerySuggested += value; }
       remove { CommandManager.RequerySuggested -= value; }
     }
-
     public bool CanExecute(object parameter) {
       double op2;
       return c.Op1 != null && double.TryParse(c.InputString, out op2)
                            && (c.Op != "/" || op2 != 0);
     }
-
     public void Execute(object parameter) {
       double op2 = double.Parse(c.InputString);
       c.InputString = calculate(c.Op, (double) c.Op1, op2).ToString();
       c.Op1 = null;
     }
-
     private static double calculate(string op, double op1, double op2) {
       switch (op) {
         case "+": return op1 + op2;
@@ -187,5 +158,9 @@ namespace Calculator {
       }
       return 0;
     }
+    public Calculate(Calculator c) {
+      this.c = c;
+    }
+    private Calculator c;
   }
 }
